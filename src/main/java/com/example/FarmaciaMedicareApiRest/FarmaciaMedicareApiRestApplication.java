95e0767c5f708640fbbcbee0d7deb9b40cb02106
EmanuelTapia <166262685+EmanuelTapia@users.noreply.github.com>
2024-07-05 19:15:32 -0500

    Initial commit

---
+++ src/main/java/com/example/FarmaciaMedicareApiRest/FarmaciaMedicareApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.FarmaciaMedicareApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaMedicareApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaMedicareApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FarmaciaMedicareApiRest/FarmaciaMedicareApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FarmaciaMedicareApiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaMedicareApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
